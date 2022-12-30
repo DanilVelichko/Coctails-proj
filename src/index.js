38faf21b42060b430716aa2ac4fe094cfd12d129
DanilVelichko <velichko.danil@gmail.com>
2022-12-30 11:29:31 +0100

    links to favorite pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+console.log('hello! im the index.js page');
+
+import "./js/favorite";
+import "./js/gallery";
+import "./js/header-search";
+import "./js/hero-letters-filter";
+import "./js/modal";
+import "./js/refs";
