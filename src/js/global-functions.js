0e9bbb02ffd9ec296ffcd370689b27012866af1e
DanilVelichko <velichko.danil@gmail.com>
2023-01-03 21:54:40 +0100

    Id Drinks

--- src/js/global-functions.js
+++ src/js/global-functions.js
@@ -1,29 +1,29 @@
 import { refs, fetchEl, point, counter } from './refs.js';
 
-export const renderCard = ({ strDrinkThumb, strDrink }) => {
+export const renderCard = ({ strDrinkThumb, strDrink, idDrink }) => {
     return `
   <li class="gallery__coctail_box">
         <picture class="gallery__coctail_img">
           <source
             srcset="
-              ${strDrinkThumb}/preview(350x350 pixels) 1x,
-              ${strDrinkThumb}/preview(350x350 pixels) 2x
+              ${strDrinkThumb} 1x,
+              ${strDrinkThumb} 2x
             "
             media="(max-width: 1280px)"
             type="image/png"
           />
           <source
             srcset="
-              ${strDrinkThumb}/preview(350x350 pixels) 1x,
-              ${strDrinkThumb}/preview(350x350 pixels) 2x
+              ${strDrinkThumb} 1x,
+              ${strDrinkThumb} 2x
             "
             media="(max-width: 768px)"
             type="image/png"
           />
           <source
             srcset="
-              ${strDrinkThumb}/preview(350x350 pixels) 1x,
-              ${strDrinkThumb}/preview(350x350 pixels) 2x
+              ${strDrinkThumb} 1x,
+              ${strDrinkThumb} 2x
             "
             media="(max-width: 480px)"
             type="image/png"
@@ -46,15 +46,14 @@ export const renderCard = ({ strDrinkThumb, strDrink }) => {
 
               src="./images/Heart-white.svg"
               alt="Heart"
-
-            />
+             
+            /><div class="coctailsId visually-hidden">${idDrink}</div>
           </button>
-        </div>
+        </div> 
       </li>
 `;
 };
   
-
 export const formatScreenRender =  (yourFetchFunction) => {
     if (window.matchMedia("(min-width: 1280px)").matches) {
         for (let i = 0; i < counter.desktop; i++) {
