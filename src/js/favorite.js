38dc9afd142592a1bbd4fd3599bc2e6dd8d17061
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-04 23:11:11 +0200

    Storage js

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,2 +1,22 @@
+// import { loadFromLocalStorage } from './global-functions';
 
-console.log('Favorite js');
+// console.log(loadFromLocalStorage());
+// console.log(serializedState);
+
+// function load(CoctailsId) {
+//   try {
+//     const serializedState = localStorage.getItem(CoctailsId);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// }
+
+// console.log(load());
+
+function Storage() {
+  const pool = JSON.parse(localStorage.getItem('CoctailsId'));
+  console.log(pool);
+}
+
+Storage();
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,28 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 import debounce from 'debounce';
-import { renderCard, formatScreenRender, cleanHTML, saveInLocalStorage, loadFromLocalStorage } from "./global-functions.js";
+import {
+  renderCard,
+  formatScreenRender,
+  cleanHTML,
+  saveInLocalStorage,
+  loadFromLocalStorage,
+} from './global-functions.js';
 import { refs, fetchEl, point, counter, localStorageArr } from './refs.js';
 
 const debouncedRender = debounce(formatScreenRender(galleryMarkup), 1000);
 
-
 async function galleryMarkup() {
-    cleanHTML();
-    try {
-        const response = await axios.get(refs.randomCoctailApi);
-        point.galleryUl
-            .insertAdjacentHTML('beforeend', renderCard(response.data.drinks[0]));
-      
-    } catch (error) {
-        console.dir(error);
-    }
-}
+  cleanHTML();
+  try {
+    const response = await axios.get(refs.randomCoctailApi);
 
+    point.galleryUl.insertAdjacentHTML(
+      'beforeend',
+      renderCard(response.data.drinks[0])
+    );
+  } catch (error) {
+    console.dir(error);
+  }
+}
 
 debouncedRender();
 window.addEventListener('resize', debouncedRender);
 
-
-
 console.log('Подключена страница  Gallery js');
-
