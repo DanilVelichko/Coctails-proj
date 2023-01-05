b1d043bc502e873a45ffb0d228133c681a67d026
DanilVelichko <velichko.danil@gmail.com>
2023-01-05 16:01:49 +0100

    Js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import "./js/pagination.js";
-import "./js/favorite-button";
-import "./js/favorite";
-import "./js/gallery";
-import "./js/header-search";
-import "./js/hero-letters-filter";
-import "./js/modal";
-import "./js/refs";
-import "./js/global-functions";
+    import start from "./js/index";
 
+document.addEventListener("DOMContentLoaded", () => {
+    start();
+});
