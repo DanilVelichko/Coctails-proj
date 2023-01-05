2e4a4cf9c72a65eff49e0c52ea72a77c69d19c54
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-05 17:43:19 +0200

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import debounce from 'debounce';
-
 import {
   renderCard,
   formatScreenRender,
@@ -12,43 +11,18 @@ import { refs, fetchEl, point, counter, localStorageArr } from './refs.js';
 
 const debouncedRender = debounce(formatScreenRender(galleryMarkup), 1000);
 
-// async function galleryMarkup() {
-//   cleanHTML();
-//   try {
-//     const response = await axios.get(refs.randomCoctailApi);
-
-//     import {
-//       renderCard,
-//       formatScreenRender,
-//       cleanHTML,
-//       saveInLocalStorage,
-//       loadFromLocalStorage,
-//     } from './global-functions.js';
-//     import { refs, fetchEl, point, counter, localStorageArr } from './refs.js';
-
-//     const debouncedRender = debounce(formatScreenRender(galleryMarkup), 1000);
-
-//     export async function galleryMarkup() {
-//       cleanHTML();
-//       try {
-//         const response = await axios.get(refs.randomCoctailApi);
-//         point.galleryUl.insertAdjacentHTML(
-//           'beforeend',
-//           renderCard(response.data.drinks[0])
-//         );
-//       } catch (error) {
-//         console.dir(error);
-//       }
-//     }
-
-//     point.galleryUl.insertAdjacentHTML(
-//       'beforeend',
-//       renderCard(response.data.drinks[0])
-//     );
-//   } catch (error) {
-//     console.dir(error);
-//   }
-// }
+export async function galleryMarkup() {
+  cleanHTML();
+  try {
+    const response = await axios.get(refs.randomCoctailApi);
+    point.galleryUl.insertAdjacentHTML(
+      'beforeend',
+      renderCard(response.data.drinks[0])
+    );
+  } catch (error) {
+    console.dir(error);
+  }
+}
 
 debouncedRender();
 window.addEventListener('resize', debouncedRender);
