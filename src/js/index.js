c7621407ff28f1cee61159ab381111bc580b2e93
DanilVelichko <velichko.danil@gmail.com>
2023-01-06 16:22:57 +0100

    favorite done

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
-import "./pagination.js";
-import "./favorite-button";
 import "./favorite";
+import "./favorite-button";
 import "./gallery";
 import "./header-search";
 import "./hero-letters-filter";
 import "./modal";
 import "./refs";
 import "./global-functions";
-
+import "./pagination.js";
