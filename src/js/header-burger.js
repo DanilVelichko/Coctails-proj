d35cc7fb2d455dad7de157e2ff0096bb39f60ae4
JuliaNiky <yuliia.nikolaienko.1609@gmail.com>
2023-01-08 01:20:51 +0200

    added media screen settings

--- src/js/header-burger.js
+++ src/js/header-burger.js
@@ -2,18 +2,18 @@ import { refs } from './refs';
 
 (function headerInit() {
   refs.backdropMenu.addEventListener('mouseleave', () => {
-    refs.menuSecondRow.classList.remove('active');
+    refs.menu.classList.remove('active');
   });
 
   refs.backdropMenu.addEventListener('mouseenter', () => {
-    refs.menuSecondRow.classList.add('active');
+    refs.menu.classList.add('active');
   });
 
-  refs.mobileMenuBurger?.addEventListener('click', closeMobileMenu);
+  refs.refHeaderMenuOpen?.addEventListener('click', closeMobileMenu);
 
   // Burger menu
 
-  document.querySelector('.burger__btn').addEventListener('click', () => {
+  document.querySelector('.nav-mobile__btn').addEventListener('click', () => {
     document.querySelector('.nav-mobile__burger').classList.add('open');
   });
 
@@ -25,7 +25,7 @@ import { refs } from './refs';
 })();
 
 export function closeMobileMenu() {
-  refs.menuSecondRow?.classList.remove('active');
+  refs.menu?.classList.remove('active');
   //   refs.refHeaderMobMenu.classList.toggle('open');
   //   refs.refHeaderSwitcher.classList.toggle('open');
   //   document.querySelector('.header__search').classList.toggle('open');
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,10 @@
 export const STORAGE_KEY = 'CoctailsId';
 
 export const refs = {
-  //header
+   //header
   backdropMenu: document.querySelector('.nav-desktop__item--js'),
-  menuSecondRow: document.querySelector('.favorite__list'),
-  mobileMenuBurger: document.querySelector('.active'),
+  menu: document.querySelector('.favorite__list'),
+  refHeaderMenuOpen: document.querySelector('.active'),
   //
   apiKey: 1,
   randomCoctailApi: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
