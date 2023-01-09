744c24e62830cb53278736b56c1429bce03b987c
JuliaNiky <yuliia.nikolaienko.1609@gmail.com>
2023-01-09 10:57:55 +0200

    adaptive nav

--- src/js/header-burger.js
+++ src/js/header-burger.js
@@ -1,29 +1,27 @@
 import { refs } from './refs';
-
 (function headerInit() {
   refs.backdropMenu.addEventListener('mouseleave', () => {
     refs.menu.classList.remove('active');
   });
-
   refs.backdropMenu.addEventListener('mouseenter', () => {
     refs.menu.classList.add('active');
   });
-
   refs.refHeaderMenuOpen?.addEventListener('click', closeMobileMenu);
-
   // Burger menu
-
   document.querySelector('.nav-mobile__btn').addEventListener('click', () => {
     document.querySelector('.nav-mobile__burger').classList.add('open');
   });
-
+document.querySelector('.nav-mobile__btn').addEventListener('click', () => {
+    document.querySelector('.nav-desktop__list').classList.add('open');
+  });
   document.querySelector('.burger__close').addEventListener('click', () => {
     document.querySelector('.nav-mobile__burger').classList.remove('open');
   });
-
+  document.querySelector('.burger__close').addEventListener('click', () => {
+    document.querySelector('.nav-desktop__list').classList.remove('open');
+  });
   // refs.refHeaderMenuClose.addEventListener('click', closeMobileMenu);
 })();
-
 export function closeMobileMenu() {
   refs.menu?.classList.remove('active');
   //   refs.refHeaderMobMenu.classList.toggle('open');
